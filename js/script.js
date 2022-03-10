3d2b1962a4711723ed4be6d709274a066c641c27
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-10 16:29:40 +0100

    Add photo

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-const settings =  {
+const app = new Vue({
     el: '#app',
     data: {
-        message: "Hello World"
+        message: "Hello World",
+        classList: "red",
+        srcImg: "https://picsum.photos/200/200",
+        altImg: "Img from lorem picsum"
     }
-};
-
-const app = new Vue(settings);
+});
